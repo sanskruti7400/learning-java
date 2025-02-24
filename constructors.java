0200d21e2c1e2201a75368cf198568ce307044f5
Sanskruti Dhanaji Kadam <sanskadam7400@gmail.com>
2025-02-24 23:02:49 +0530

    Update constructors.java

--- constructors.java
+++ constructors.java
@@ -1,18 +1,1 @@
-class Student{
-    String name;
-    int age;
-    public void printInfo(){
-        System.out.println(this.name);
-        System.out.println(this.age);
-    }
-    Student(String name,int age){
-        this.name=name;
-        this.age=age;
-    }
-}
-class Constructors{
-    public static void main(String args[]){
-        Student s1=new Student("arti",20);
-        s1.printInfo();
-    }
-}
+
