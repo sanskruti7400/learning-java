972943989388a3b4428778bb85014306fa07ca64
Sanskruti Dhanaji Kadam <sanskadam7400@gmail.com>
2025-02-25 21:21:31 +0530

    Update constructorOverloading.java

--- constructorOverloading.java
+++ constructorOverloading.java
@@ -11,6 +11,10 @@ class Student{
     Student(int prn){
         this.prn=prn;
     }
+    Student(int age,String name){
+        this.name=name;
+        this.age=age;
+    }
     Student(int age,String name,int prn){
         this.name=name;
         this.age=age;
@@ -20,6 +24,10 @@ class Student{
 class ConstructorOverloading{
     public static void main(String args[]){
         Student s1=new Student(21,"sanskruti",1234);
+        Student s2=new Student(21,"arti");
+        Student s3=new Student(1234);
         s1.printInfo();
+        s2.printInfo();
+        s3.printInfo();
     }
 }
